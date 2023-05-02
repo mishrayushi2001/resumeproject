a92e570929b2fd757def882d0baa7497e4929ffb
mishrayushi2001 <mishrayushi2000@gmail.com>
2023-05-02 13:35:35 +0530

    project

--- core/views.py
+++ core/views.py
@@ -5,4 +5,6 @@ def home(request):
     return render(request,"core/home.html",context)
 
 def contact(request):
-    return render(request,"core/contact.html")
+    context={'contact':'active'}
+
+    return render(request,"core/contact.html", context)
