dacf022218228594d213181c642076f5e589f341
Km Ayushi Mishra <56318124+mishrayushi2001@users.noreply.github.com>
2023-05-08 21:14:16 +0530

    commented line 24

--- resumeproject/urls.py
+++ resumeproject/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
     path('contact/',views.contact,name="contact"),
-    path('serv/', include('serv.urls')),
+#     path('serv/', include('serv.urls')),
     path('edu/', include('edu.urls')),
 ]
